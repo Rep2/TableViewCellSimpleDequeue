6a88ac9f5efa9400824f28cb606c431ec32e5ca9
Ivan Rep <ivan.rep2@gmail.com>
2018-10-26 16:16:02 +0200

    Initial commit

---
+++ TableViewCellSimpleDequeue/TableViewCellSimpleDequeue.h
@@ -0,0 +1,19 @@
+//
+//  TableViewCellSimpleDequeue.h
+//  TableViewCellSimpleDequeue
+//
+//  Created by Ivan Rep on 26/10/2018.
+//  Copyright © 2018 Ivan Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableViewCellSimpleDequeue.
+FOUNDATION_EXPORT double TableViewCellSimpleDequeueVersionNumber;
+
+//! Project version string for TableViewCellSimpleDequeue.
+FOUNDATION_EXPORT const unsigned char TableViewCellSimpleDequeueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableViewCellSimpleDequeue/PublicHeader.h>
+
+
